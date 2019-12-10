dd3eb4f7de8298793daf02a08e1badf3a65d9194
rubonics <rubenrodriguez+github@gmail.com>
2019-12-09 19:09:09 -0800

    adds collapsing side nav. adds @risd/ui package to power the nav. adds main-nav-v2 script and style to get the desired interaction and aesthetic.

--- script/src/index.js
+++ script/src/index.js
@@ -2,11 +2,23 @@ global.jQuery = require('jquery');
 global.$ = global.jQuery;
 global._ = require('lodash');
 
+var { hydrateComponents } = require('@risd/react-hydrator');
+var { themes } = require('@risd/ui');
+
+// Hydrate React components
+hydrateComponents(themes.congratulationsV2);
+
+// unhide nav element once it's hydrated
+const navElement = document.querySelector('[data-react-component="Nav"]');
+navElement.style.display = 'block';
+
+var mainNav = require( './main-nav-v2' )()
+
 // var exampleModule = require('./exampleModule.js')();
-var stickyNav = require('./stickyNav.js')();
-var mobileMenuToggle = require('./mobileMenuToggle.js')();
-var desktopMenuToggle = require('./desktopMenuToggle.js')();
-var desktopNavHeight = require('./desktopNavHeight.js')();
+// var stickyNav = require('./stickyNav.js')();
+// var mobileMenuToggle = require('./mobileMenuToggle.js')();
+// var desktopMenuToggle = require('./desktopMenuToggle.js')();
+// var desktopNavHeight = require('./desktopNavHeight.js')();
 var resourceImageSort = require('./resourceImageSort.js')();
 var wysiwygLightbox = require('./lightbox.js')();
 /*var alumniImageLightbox = require('./lightbox.js')({
