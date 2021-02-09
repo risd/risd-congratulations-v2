328273a2f1474868de3c99dff828f4a58f23c39f
rubonics <rubenrodriguez+github@gmail.com>
2021-02-09 14:51:24 -0500

    adds tape and generate test aimed to identify regressions in lodash

--- test/generate.js
+++ test/generate.js
@@ -1,171 +1,137 @@
-var assert = require("assert");
-var fs = require('fs');
+/**
+ * the purpose of this test is to ensure that we aren't loosing
+ * any objects in the swig representation of the cms.
+ *
+ * to do this, we should be able to write a template that counts
+ * how many objects are in each content type. then verify that
+ * number directly in the js object represetation of the data.
+ */
 
-var config = {
-  get: function(key) {
-    return this[key];
-  },
-  webhook: {
-    firebase: 'webhook',
-    secretKey: '5e13aef1-8aa8-41b4-8619-2eaf62c0ae49',
-    siteName: 'test'
-  },
-  connect: {
-    'wh-server': {
-      options: {
-        livereload: 35730
-      }
+const test = require( 'tape' )
+const exec = require( 'child_process' ).execSync
+const fs = require( 'fs' )
+
+// prep swig
+var swig = require( 'swig' )
+swig.setDefaults( {
+  loader: swig.loaders.fs(__dirname + '/..')
+})
+var swigFunctions = require('../libs/swig_functions').swigFunctions()
+var swigFilters = require('../libs/swig_filters')
+var swigTags = require('../libs/swig_tags')
+swigFilters.init( swig )
+swigTags.init( swig )
+swig.setDefaults({ cache: false })
+
+test( 'download-data', function ( t ) {
+
+  t.plan( 1 )
+
+  const output = exec( 'grunt download-data' ).toString()
+
+  t.assert( output.indexOf( 'Done, without errors.' ) > 0, 'downloaded-data' )
+} )
+
+test( 'compare-cms-object-to-downloaded-data', function ( t ) {
+  const data = JSON.parse(
+      fs.readFileSync( './.build/data.json' )
+        .toString()
+  )
+
+  // set data on swig
+  swigFunctions.setData( data.data )
+  swigFunctions.setTypeInfo( data.typeInfo )
+  swigFunctions.setSettings( data.settings )
+  swigFilters.setTypeInfo( data.typeInfo )
+
+  const multipleContentTypes = allMultipleContentTypes( data )
+
+  t.plan( multipleContentTypes.length )
+
+  // create test template
+  const testTemplateStrings = multipleContentTypes.map( function ( type ) {
+    return `
+      {% set count_${ type } = 0 %}
+      {% for item in cms.${ type } %}
+        {% set count_${ type } = count_${ type } + 1 %}
+      {% endfor %}
+      {{ '${ type }:' + count_${ type } }}
+    `
+  } )
+
+  const testTemplateString = testTemplateStrings.join( '\n' )
+
+  // setup swig params
+  const params = Object.assign( { emitter: true }, swigFunctions.getFunctions() )
+  swigFunctions.init()
+
+  const testTemplateOutput = swig.render( testTemplateString, { locals: params } )
+
+  const testTemplateResults = testTemplateOutput
+    .split( '\n' )
+    .filter( function ( line ) {
+      return line.trim().length > 0
+    } )
+    .map( function ( line ) {
+      const result = {}
+      const resultArray = line.trim().split( ':' )
+      result[ resultArray[ 0 ] ] = +resultArray[ 1 ]
+      return result
+    } )
+    .reduce( function ( previous, current ) {
+      const d = Object.assign( {}, previous, current )
+      return d
+    }, {} )
+
+  const downloadedResults = multipleContentTypes
+    .map( function ( type ) {
+      const result = {}
+      const items = Object.keys( data.data[ type ] )
+        .map( function ( itemKey ) {
+          return data.data[ type ][ itemKey ]
+        } )
+      const publishedItems = items
+        .filter( function ( item ) {
+          // as the generator does, remove items with no publish
+          // date, or one that is in the future
+          if ( ! item.publish_date ) {
+            return false
+          }
+
+          var now = Date.now()
+          var pdate = Date.parse( item.publish_date )
+
+          if ( pdate > now + 1 * 60 * 1000 ) {
+            return false
+          }
+
+          return true
+        } )
+      result[ type ] = publishedItems.length
+      return result
+    } )
+    .reduce( function ( previous, current ) {
+      const d = Object.assign( {}, previous, current )
+      return d
+    }, {} )
+
+  multipleContentTypes.forEach( function ( type ) {
+    t.assert(
+      testTemplateResults[ type ] === downloadedResults[ type ],
+      `${ type } count should match - ${ testTemplateResults[ type ] } == ${ downloadedResults[ type ] }`
+    )
+  } )
+} )
+
+
+function allMultipleContentTypes ( data ) {
+  const contentTypes = []
+
+  Object.keys( data.typeInfo ).forEach( function ( type ) {
+    if ( data.typeInfo[ type ].oneOff === false ) {
+      contentTypes.push( type )
     }
-  }
+  } )
+
+  return contentTypes
 }
-
-var fileParser = {
-  readJSON: function(file) {
-    var data = fs.readFileSync(file);
-
-    return JSON.parse(data);
-  }
-}
-
-var generator = require('../libs/generator.js').generator(config, {}, null, fileParser);
-
-// Set up all the data here
-
-describe('generator', function(){
-  describe('#renderPages(done, cb)', function(){
-    before(function(done) {
-      generator.renderPages(done, function(cb) { cb(); });
-    })
-    it('should render html files in the pages directory', function(){
-    })
-    it('should render robots.txt in the pages directory', function(){
-    })
-    it('should render _supported.js in the build directory', function(){
-    })
-    it('should render XML, RSS, XHTML, and ATOM in the pages directory', function(){
-    })
-  })
-})
-
-describe('generator', function(){
-  describe('#renderTemplates(done, cb)', function(){
-    before(function(done) {
-      generator.renderTemplates(done, function(cb) { cb(); });
-    })
-    // Check .slug vs not .slug
-    // Need Three Types. One that has no custom urls, type url only, invidivual url only, and both
-    it('should render the templates for the site', function(){
-    })
-    it('should handle no custom slugs', function(){
-    })
-    it('should handle custom type slugs', function(){
-    })
-    it('should handle custom item slugs', function(){
-    })
-    it('should handle custom type and item slugs', function(){
-    })
-    it('should handle sub templates', function(){
-    })
-  })
-})
-
-describe('generator', function(){
-  describe('#copyStatic(callback)', function(){
-    it('should copy the static files to build', function(){
-    })
-  })
-})
-
-describe('generator', function(){
-  describe('#cleanFiles(done, cb)', function(){
-    it('should remove the build folder', function(){
-    })
-  })
-})
-
-describe('generator', function(){
-  describe('#buildBoth(done, cb)', function(){
-    it('should render both the pages and templates', function(){
-    })
-  })
-})
-
-describe('generator', function(){
-  describe('#checkScaffoldingMD5(name, callback)', function(){
-    it('should return the MD5s for templates', function(){
-    })
-  })
-})
-
-describe('generator', function(){
-  describe('#makeScaffolding(name, done, force)', function(){
-    it('should generate scaffolding for a given type', function(){
-    })
-  })
-})
-
-describe('generator', function(){
-  describe('#reloadFiles(done)', function(){
-    it('should send reload signal to live reload', function(){
-    })
-  })
-})
-
-describe('generator', function(){
-  describe('#startLiveReload()', function(){
-    it('should start live reload server', function(){
-    })
-  })
-})
-
-describe('generator', function(){
-  describe('#webListener()', function(){
-    // Test all the different messages here
-    // Create a websocket to talk to web listener, will use in sendSockMessage as well
-    it('should render the templates for the site', function(){
-    })
-  })
-})
-
-describe('generator', function(){
-  describe('#sendSockMessage(message)', function(){
-    it('should send a message over websocket', function(){
-    })
-  })
-})
-
-
-describe('generator', function(){
-  describe('#init(firebaseConfOptions, copyCms, done)', function(){
-    it('should initialized the firebase conf and cms', function(){
-    })
-  })
-})
-
-describe('generator', function(){
-  describe('#assets(grunt)', function(){
-    it('should render the templates for the site', function(){
-    })
-  })
-})
-
-describe('generator', function(){
-  describe('#assetsMiddle(grunt)', function(){
-    it('should render the templates for the site', function(){
-    })
-  })
-})
-
-describe('generator', function(){
-  describe('#assetsAfter(grunt)', function(){
-    it('should render the templates for the site', function(){
-    })
-  })
-})
-
-describe('generator', function(){
-  describe('#enableStrictMode()', function(){
-    it('should enable strict mode in generator', function(){
-    })
-  })
-})
